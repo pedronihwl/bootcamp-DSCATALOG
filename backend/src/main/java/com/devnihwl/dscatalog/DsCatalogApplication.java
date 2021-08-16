eafde63f281c307b50a3d680be5236ff90fb4479
Pedro Pereira <75137454+NihwlCat@users.noreply.github.com>
2021-08-16 20:55:21 -0300

    Testing github dot feature

--- backend/src/main/java/com/devnihwl/dscatalog/DsCatalogApplication.java
+++ backend/src/main/java/com/devnihwl/dscatalog/DsCatalogApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsCatalogApplication {
 
+	// Sério que o GitHub agora faz isso?
+
 	public static void main(String... args) {
 		SpringApplication.run(DsCatalogApplication.class, args);
 	}
